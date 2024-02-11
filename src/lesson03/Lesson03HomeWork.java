4fde2def4de2226882b60dcdf29e7284081b82ce
Dmitry <smirnoff.spb78@yandex.ru>
2024-02-11 17:57:25 +0300

    Удален костыль из задания 1, все числа теперь целочисленные В задании 3 явно учтена високосность в зависимости от значения года В задании 4 не введена переменная resultSumWithoutDiscount Переименована директория

--- src/lesson03/Lesson03HomeWork.java
+++ src/lesson03/Lesson03HomeWork.java
@@ -1,25 +1,23 @@
-package lessonthree;
+package lesson03;
 
 public class Lesson03HomeWork {
     public static void main(String[] args) {
-        /*Задача #1 на Math.random() и ветвление
+        /** Задача #1 на Math.random() и ветвление
         Самостоятельно изучить Math.random() для генерации псевдослучайных чисел.
         Написать код, который будет проверять попало ли случайно сгенерированное целое число из отрезка [10;500] в интервал (25;200) и выводить результат в консоль.
         Примеры работы программы: Число 345 не содержится в интервале (25;200) Число 110 содержится в интервале (25;200)*/
-        var random = Math.random() * 491 + 10;
-        System.out.print("Задание 1\nЧисло "+random);
-        random = random > 500 ? 500 : random;
+        int random =(int) (Math.random() * 491 + 10);
+        System.out.print("Задание 1\nЧисло " + random);
         if (random > 25 && random < 200) System.out.println(" попало в интервал (25; 200)");
         else System.out.println(" не попало в интервал (25; 200)");
 
-
-      /*  Задача #2 на if else
+      /**  Задача #2 на if else
         Даны переменные x и y - координаты точки на плоскости.
                 Необходимо определить четверть координатной плоскости, которой принадлежит данная точка. Результат вывести в консоль.
         Если обе координаты положительны, точка принадлежит первой четверти.
                 Если координата x отрицательна, а y положительна, точка принадлежит второй четверти.
                 Если обе координаты отрицательны, точка принадлежит третьей четверти.
-                Если координата x положительна, а y отрицательна, точка принадлежит четвёртой четверти.*/
+                Если координата x положительна, а y отрицательна, точка принадлежит четвёртой четверти.**/
         System.out.println("\nЗадание 2");
         int coordinateX = 10;
         int coordinateY = -20;
@@ -29,14 +27,14 @@ public class Lesson03HomeWork {
         else if (coordinateX > 0 && coordinateY < 0) System.out.println("Точка принадлежит четвертой четверти");
         else System.out.println("Точка не принадлежит ни одной четверти");
 
-        /* Задача #3 на switch
+        /** Задача #3 на switch
         Дана переменная monthNumber, которая хранит порядковый номера месяца. И переменная isLeap, которая хранит информацию о том, является ли год високосным.
 
                 Написать программу, которая в зависимости от значения monthNumber выводит в консоль количество дней в этом месяце. Необходимо рассмотреть два случая: год не является високосным и год является високосным.*/
         System.out.println("\nЗадание 3");
         int monthNumber = 2;
-        int numberOfDayInYear = 366;
-        if (numberOfDayInYear == 365 || numberOfDayInYear == 366) {
+        int year = 1812;
+        int numberOfDayInYear = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ? 366 : 365;
             System.out.print("Количество дней в месяце ");
             switch (monthNumber) {
                 case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(31);
@@ -47,9 +45,8 @@ public class Lesson03HomeWork {
                 }
                 default -> System.out.println("Неверно введен номер месяца");
             }
-        } else System.out.println("Неверное количество дней в году");
 
-        /*  Задача #4 на switch
+        /**  Задача #4 на switch
         Даны переменные couponNumber - номер купона и resultSum - сумма покупки.
                 Если номер купона - 1111, 4444 или 9999 - скидка 10%,
                 Если номер купона - 3333 или 7777 - скидка 20%,
@@ -57,12 +54,12 @@ public class Lesson03HomeWork {
         В остальных случаях скидка не предусмотрена.
         Необходимо вычислить и сохранить в переменную resultSum сумму с учетом скидки, если она предусмотрена. Вывести значение resultSum в консоль.*/
         int couponNumber = 5555;
-        double resultSum = 157.5;
-        resultSum = switch (couponNumber) {
-            case 1111, 4444, 9999 -> 0.9 * resultSum;
-            case 3333, 7777 -> 0.8 * resultSum;
-            case 5555 -> 0.7 * resultSum;
-            default -> resultSum;
+        double resultSumWithoutDiscount = 157.5;
+        double resultSum = switch (couponNumber) {
+            case 1111, 4444, 9999 -> 0.9 * resultSumWithoutDiscount;
+            case 3333, 7777 -> 0.8 * resultSumWithoutDiscount;
+            case 5555 -> 0.7 * resultSumWithoutDiscount;
+            default -> resultSumWithoutDiscount;
         };
         System.out.println("\nЗадание 4 \nСумма покупки " + resultSum);
     }
