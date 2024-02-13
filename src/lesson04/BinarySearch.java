fb0a4156ad8971eec2142610f48669026ce99dde
Dmitry <smirnoff.spb78@yandex.ru>
2024-02-13 22:15:22 +0300

    метод binarySearchInt() исправлен с defuelt на public. Исправлены элементы оформления

--- src/lesson04/BinarySearch.java
+++ src/lesson04/BinarySearch.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class BinarySearch {
     private BinarySearch() {
     }
-     static int binarySearchInt() {
+
+    public static int binarySearchInt() {
         int userInput;
         Scanner scanner = new Scanner(System.in);
         int rightNumber = 101;
         int leftNumber = 0;
         int programNumber = 50;
         System.out.println("Придумайте цклое число от 1 до 100");
-        while (rightNumber != leftNumber + 2 ) {
+        while (rightNumber != leftNumber + 2) {
             System.out.println("Чило равно " + programNumber + "?\n1 - Да\n0 - Нет");
             userInput = scanner.nextInt();
             if (userInput == 0) {
@@ -24,9 +25,14 @@ public class BinarySearch {
                 } else if (userInput == 0) {
                     rightNumber = programNumber;
                     programNumber -= (rightNumber - leftNumber) / 2;
-                } else System.out.println("Неверная подсказка");
-            } else if (userInput == 1) break;
-            else System.out.println("Неверная подсказка");
+                } else {
+                    System.out.println("Неверная подсказка");
+                }
+            } else if (userInput == 1) {
+                break;
+            } else {
+                System.out.println("Неверная подсказка");
+            }
         }
         return programNumber;
     }
