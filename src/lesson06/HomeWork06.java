5aa40b5d6e96807ee1e3bf891799f23b08cbed54
Dmitry <smirnoff.spb78@yandex.ru>
2024-02-19 23:06:23 +0300

    Исправлены ошибки ДЗ №6 Добавлен вывод в логи Исправлены условные блоки, добавленные в предыдущий коммит Исправлены элементы оформления (добавлены описания к классам и полям метода, исправлены шрифты в коде)

--- src/lesson06/HomeWork06.java
+++ src/lesson06/HomeWork06.java
@@ -1,7 +1,5 @@
 package lesson06;
 
-import java.util.Arrays;
-
 import static lesson06.BubbleMethod.bubbleMethod;
 import static lesson06.SlidingWindow.slidingWindow;
 
@@ -10,26 +8,11 @@ public class HomeWork06 {
         /**Прочитать и реализовать пузырьковый метод
          */
         int[] arrayForBubbleMethod = {5, 2, 3, 4};
-        int[] sortedArray;
-        if (arrayForBubbleMethod == null) {
-            System.out.println("В метод sortedArray передан null");
-            sortedArray = new int[1];
-        } else {
-            sortedArray = new int[arrayForBubbleMethod.length];
-            System.arraycopy(arrayForBubbleMethod, 0, sortedArray, 0, arrayForBubbleMethod.length);
-        }
-        bubbleMethod(sortedArray);
-        System.out.println(Arrays.toString(sortedArray));
+        bubbleMethod(arrayForBubbleMethod);
 
         /**Дан массив целых чисел: `int[] task02 = {88, 34, 12, 55, 90, 4, 10, 44}`. Найти максимальную сумму четырёх смежных целых
          чисел в массиве task02. Использовать алгоритм скользящего окна.*/
         int[] task02 = {88, 34, 12, 55, 90, 4, 10, 44};
-        if (task02 == null) {
-            System.out.println("В метод SlidingWindow пердан null");
-            int[] exeptionArray = new int[3];
-            System.out.println(slidingWindow(exeptionArray));
-        } else {
-            System.out.println(slidingWindow(task02));
-        }
+        slidingWindow(task02);
     }
 }
