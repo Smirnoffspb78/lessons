047f13c40ecde15649ff3638fe2e86c11dac6fa5
Dmitry <smirnoff.spb78@yandex.ru>
2024-02-19 23:06:23 +0300

    Исправлены ошибки ДЗ №6 Добавлен вывод в логи Исправлены условные блоки, добавленные в предыдущий коммит Исправлены элементы оформления (добавлены описания к лассам и полям метода, исправлены шрифты в коде)

--- src/lesson06/SlidingWindow.java
+++ src/lesson06/SlidingWindow.java
@@ -1,23 +1,34 @@
 package lesson06;
 
+import java.util.logging.Logger;
+
 import static java.lang.Math.max;
 
+/**
+ * Класс, реализующий метод скользящего окна для четырех элементов массива
+ */
 public class SlidingWindow {
     private SlidingWindow() {
     }
 
+    private static final Logger LOGGER = Logger.getLogger(SlidingWindow.class.getName());
+    public static final int NUMBER = 4; //Число элементов массива, для поиска максимума
+
     public static int slidingWindow(int[] arr) {
-        if (arr.length<4){
-            System.out.println("В метод SlidingWindow передан некорректный массив");
+        if (arr == null) {
+            LOGGER.warning("В метод slidingWindow передан null");
             return 0;
+        } else if (arr.length < 4) {
+            LOGGER.warning("В метод slidingWindow передан массив с длинной меньше 4");
+            return 0;
+        } else {
+            var maxSumma = 0;
+            for (int i = 0; i < arr.length; i++) {
+                maxSumma = i < NUMBER ? maxSumma + arr[i] : max(maxSumma, maxSumma - arr[i - NUMBER] + arr[i]);
+            }
+            String message = "Максимальная сумма четырех чисел: " + maxSumma;
+            LOGGER.info(message);
+            return maxSumma;
         }
-        else {
-        var maxSumma = 0;
-        final var number = 4;
-        for (int i = 0; i < arr.length; i++) {
-            maxSumma = i < number ? maxSumma + arr[i] : max(maxSumma, maxSumma - arr[i - number] + arr[i]);
-        }
-        System.out.print("Максимальная сумма четырех чисел: ");
-        return maxSumma;}
     }
 }
