3bc943882bff50ce0b465d75992f560af0cd850e
Dmitry <smirnoff.spb78@yandex.ru>
2024-03-16 11:19:19 +0300

    Исправлены ошибки ДЗ №14

--- src/com/smirnov/project/lesson14/homework14/task2/Main.java
+++ src/com/smirnov/project/lesson14/homework14/task2/Main.java
@@ -1,20 +1,27 @@
 package com.smirnov.project.lesson14.homework14.task2;
 
-import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Double[] doubles = {1.0, 2.0, 3.0, 4.0, 5.0}; //2+4=6; 1+3+5=9
 
-        ReturnPairContainer<Double> ret = new ReturnPairContainer<>();
-        System.out.println(ret.returnPairContainer(doubles));
+        PairContainer<Double> pairContainer=PairContainer.generatePairContainer(doubles);
+        System.out.println(pairContainer);
+        Double key1d=pairContainer.getSumOfEvenNumbers();
+        Double key2d=pairContainer.getSumOfOddNumbers();
+        System.out.println("1: "+key1d+"  2: "+key2d);
 
         Integer[] integers = {1, 2, 3, 4, 5};
-        ReturnPairContainer<Integer> ret1 = new ReturnPairContainer<>();
-        System.out.println(ret1.returnPairContainer(integers));
+        PairContainer<Integer> pairContainer1=PairContainer.generatePairContainer(integers);
+        System.out.println(pairContainer1);
+        Integer key1=pairContainer1.getSumOfEvenNumbers();
+        Integer key2=pairContainer1.getSumOfOddNumbers();
+        System.out.println("1: "+key1+"  2: "+key2);
 
-
-        BigDecimal[] bigDecimals={BigDecimal.valueOf(1),BigDecimal.valueOf(1),BigDecimal.valueOf(2),BigDecimal.valueOf(3)};
-
+        Double[] doubles1=new Double[0];
+        System.out.println(Arrays.toString(doubles1));
+        PairContainer<Double> pairContainer2=PairContainer.generatePairContainer(doubles1);
+        System.out.println(pairContainer2);
     }
 }
--- src/com/smirnov/project/lesson14/homework14/task2/PairContainer.java
+++ src/com/smirnov/project/lesson14/homework14/task2/PairContainer.java
@@ -1,13 +1,12 @@
 package com.smirnov.project.lesson14.homework14.task2;
 
 /**
- * Контейнер, хранящий пару ключей
- *
- * @param <T> Number
- * @param sumOfEvenNumbers Ключ №1.
- * @param sumOfOddNumbers  Ключ №2.
+ * Контейнер, хранящий пару ключей.
  */
-public record PairContainer<T extends Number>(T sumOfEvenNumbers, T sumOfOddNumbers) {
+public final class PairContainer<T extends Number> {
+    private final T sumOfEvenNumbers;
+    private final T sumOfOddNumbers;
+
 
     /**
      * Конструктор создает контейнер.
@@ -15,14 +14,62 @@ public record PairContainer<T extends Number>(T sumOfEvenNumbers, T sumOfOddNumb
      * @param sumOfEvenNumbers Ключ №1
      * @param sumOfOddNumbers  Ключ №2
      */
-    public PairContainer {
+    private PairContainer(T sumOfEvenNumbers, T sumOfOddNumbers) {
         if (sumOfEvenNumbers == null || sumOfOddNumbers == null) {
             throw new NullPointerException("SumOfEvenNumbers=null, SumOfOddNumbers=null");
         }
+        this.sumOfEvenNumbers = sumOfEvenNumbers;
+        this.sumOfOddNumbers = sumOfOddNumbers;
+    }
+
+    /**
+     * Добавляет в контейнер ключ1 - сумма четных чисел, в контейнер 2 - сумма нечетных чисел.
+     *
+     * @param arr Массив чисел
+     * @return Контейнер с ключами
+     */
+    public static <T extends Number> PairContainer<T> generatePairContainer(T[] arr) {
+        if (arr == null) {
+            throw new NullPointerException("arr=null");
+        }
+        if (arr.length == 0) {
+            return (PairContainer<T>) new PairContainer<>(0, 0);
+        }
+        double sumOfEvenNumbers = 0.0;
+        double sumOfOddNumbers = 0.0;
+        for (T t : arr) {
+            if (t == null) throw new NullPointerException("arr[i]=null");
+            if ((t.doubleValue() % 2) == 0) {
+                sumOfEvenNumbers += t.doubleValue();
+            } else {
+                sumOfOddNumbers += t.doubleValue();
+            }
+        }
+
+        if (arr[0] instanceof Byte) {
+            return (PairContainer<T>) new PairContainer<>((byte) sumOfEvenNumbers, (byte) sumOfOddNumbers);
+        } else if (arr[0] instanceof Integer) {
+            return (PairContainer<T>) new PairContainer<>((int) sumOfEvenNumbers, (int) sumOfOddNumbers);
+        } else if (arr[0] instanceof Long) {
+            return (PairContainer<T>) new PairContainer<>((long) sumOfEvenNumbers, (long) sumOfOddNumbers);
+        } else if (arr[0] instanceof Short) {
+            return (PairContainer<T>) new PairContainer<>((short) sumOfEvenNumbers, (short) sumOfOddNumbers);
+        } else if (arr[0] instanceof Float) {
+            return (PairContainer<T>) new PairContainer<>((float) sumOfEvenNumbers, (float) sumOfOddNumbers);
+        }
+        return (PairContainer<T>) new PairContainer<>(sumOfEvenNumbers, sumOfOddNumbers);
     }
 
     @Override
     public String toString() {
         return "Cумма четных числе: %s, Сумма нечетных чисел: %s".formatted(sumOfEvenNumbers, sumOfOddNumbers);
+    }
+
+    public T getSumOfEvenNumbers() {
+        return sumOfEvenNumbers;
+    }
+
+    public T getSumOfOddNumbers() {
+        return sumOfOddNumbers;
     }
 }
